4992f331eb1dc61aafcfd0f250dcffc024275705
Camilo Barrios Villamizar <malditagaseosa@gmail.com>
2019-03-17 14:58:45 -0500

    pagination corrections

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ var app = new Vue({
         pagina: 1,
         porPagina: 5,
         paginas: [],
+        listadoFiltrado: []
 
     },
     created: function() {
@@ -29,7 +30,7 @@ var app = new Vue({
     },
     methods: {
         handleSave: function() {
-            let _this = this;
+            let vm = this;
             let producto = Producto(this.form.producto, this.form.cantidad, JSON.parse(this.form.bodega), JSON.parse(this.form.estado), this.form.observacion, this.form.key);
             if(producto.key){
                 Swal.fire({
@@ -43,8 +44,8 @@ var app = new Vue({
                 }).then((result) => {
                     if(result.value){
                         DatabaseService.setProducto(producto).then(()=>{
-                            _this.form = {};
-                            _this.loadListadoProductos();
+                            vm.form = {};
+                            vm.loadListadoProductos();
                             Swal.fire(
                                 'Guardado',
                                 'Los cambios han sido guardados',
@@ -55,8 +56,8 @@ var app = new Vue({
                 });
             }else{
                 DatabaseService.setProducto(producto).then(()=>{
-                    _this.form = {};
-                    _this.loadListadoProductos();
+                    vm.form = {};
+                    vm.loadListadoProductos();
                     Swal.fire(
                         'Guardado',
                         'Se ha guardado el producto',
@@ -67,7 +68,6 @@ var app = new Vue({
         },
         handleUpdate: function(registro) {
             $('#modal-producto').modal('show');
-            console.log('update', registro);
             this.form = {
                 producto: registro.producto,
                 cantidad: registro.cantidad,
@@ -93,38 +93,36 @@ var app = new Vue({
             this.bodegas = Object.keys(snapshot.val()).map((el)=>{
                 return snapshot.val()[el];
             });
-            console.log(this.bodegas);
         },
         setListadoEstados: function(snapshot) {
             this.estados = Object.keys(snapshot.val()).map((el)=>{
                 return snapshot.val()[el];
             });
-            console.log(this.estados);
         },
         setListadoProductos: function(snapshot) {
             if(snapshot.val()){
                 this.productos = Object.keys(snapshot.val()).map((el)=>{
                     return snapshot.val()[el];
                 });
-                console.log(this.productos, this.paginas);
+                this.listadoFiltrado = this.productos;
             }else{
                 return
             }
         },
-        setPaginas: function() {
+        setPaginas: function(listado) {
             this.paginas = [];
-            let num_paginas = Math.ceil(this.productos.length/this.porPagina);
+            let num_paginas = Math.ceil(listado.length/this.porPagina);
             for (let i = 1; i <= num_paginas; i++){
                 this.paginas.push(i);
             };
-            console.log('set paginas', this.paginas);
         },
-        paginacion: function(productos) {
+        paginacion: function(listado) {
             let pagina = this.pagina;
             let porPagina = this.porPagina;
             let inicio = (pagina * porPagina) - porPagina;
             let final = (pagina * porPagina);
-            return productos.slice(inicio, final);
+            
+            return listado.slice(inicio, final);
         },
         error: function(error){
             console.log(error);
@@ -132,14 +130,17 @@ var app = new Vue({
     },
     computed: {
         productosFiltrados: function(){
-            return this.paginacion(this.productos).filter(p => {
+            return this.productos.filter(p => {
                 return p.producto.toLowerCase().indexOf(this.busqueda.toLowerCase()) > -1;
             });
         }
     },
     watch: {
         productos: function(){
-            this.setPaginas();
+            this.setPaginas(this.productos);
+        },
+        productosFiltrados: function(){
+            this.setPaginas(this.productosFiltrados);
         }
     }
   })
